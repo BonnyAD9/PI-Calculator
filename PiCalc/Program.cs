394d5a325266f5288d4c5c67cdfe5a2ffb4a02d4
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-11-03 10:05:01 +0100

    Make code less indented

--- PiCalc/Program.cs
+++ PiCalc/Program.cs
@@ -2,36 +2,35 @@
 using System.IO;
 using System.Text;
 
-namespace PiCalc
+namespace PiCalc;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // 100000 518 s 20 MB RAM
+        Console.Write("Enter here how many digits of pi you want to calculate (including 3): ");
+        int digits = 0;
+        while (true)
         {
-            // 100000 518 s 20 MB RAM
-            Console.Write("Enter here how many digits of pi you want to calculate (including 3): ");
-            int digits = 0;
-            while (true)
+            while (!int.TryParse(Console.ReadLine(), out digits))
             {
-                while (!int.TryParse(Console.ReadLine(), out digits))
-                {
-                    Console.WriteLine("That is invalid number");
-                    Console.Write("Enter valid number: ");
-                }
-                if ((digits > 100000) || (digits <= 0))
-                {
-                    Console.WriteLine("This number is out of range");
-                    Console.Write("Enter number between 0 and 100001: ");
-                }
-                else break;
+                Console.WriteLine("That is invalid number");
+                Console.Write("Enter valid number: ");
             }
-            Console.Write("Calculating (this may take up to 10 minutes)... ");
-            PI pi = new PI(digits);
-            StringBuilder sb = new StringBuilder();
-            foreach (byte b in pi.GetPi())
-                sb.Append(b);
-            File.WriteAllText($"pi{digits}digits.txt", sb.ToString());
-            Console.WriteLine($"Done!\nDigits of pi are stored in 'pi{digits}digits.txt'");
+            if ((digits > 100000) || (digits <= 0))
+            {
+                Console.WriteLine("This number is out of range");
+                Console.Write("Enter number between 0 and 100001: ");
+            }
+            else break;
         }
+        Console.Write("Calculating (this may take up to 10 minutes)... ");
+        PI pi = new PI(digits);
+        StringBuilder sb = new StringBuilder();
+        foreach (byte b in pi.GetPi())
+            sb.Append(b);
+        File.WriteAllText($"pi{digits}digits.txt", sb.ToString());
+        Console.WriteLine($"Done!\nDigits of pi are stored in 'pi{digits}digits.txt'");
     }
 }
