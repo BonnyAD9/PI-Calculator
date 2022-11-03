299ca01441c5566f8742c2ce3ae93475ad9e53a0
Jakub Antonin Stigler <jakubstig@gmail.com>
2022-11-03 12:41:24 +0100

    Add arguments

--- PiCalc/Program.cs
+++ PiCalc/Program.cs
@@ -8,29 +8,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        // 100000 518 s 20 MB RAM
-        Console.Write("Enter here how many digits of pi you want to calculate (including 3): ");
-        int digits = 0;
-        while (true)
+        if (args.Length != 1)
         {
-            while (!int.TryParse(Console.ReadLine(), out digits))
-            {
-                Console.WriteLine("That is invalid number");
-                Console.Write("Enter valid number: ");
-            }
-            if ((digits > 100000) || (digits <= 0))
-            {
-                Console.WriteLine("This number is out of range");
-                Console.Write("Enter number between 0 and 100001: ");
-            }
-            else break;
+            Console.WriteLine("Invalid number of arguments");
+            return;
         }
-        Console.Write("Calculating (this may take up to 10 minutes)... ");
+
+        if (!int.TryParse(args[0], out int digits) || digits <= 0)
+        {
+            Console.WriteLine("That is invalid a number");
+            return;
+        }
+
         PI pi = new PI(digits);
-        StringBuilder sb = new StringBuilder();
         foreach (byte b in pi.GetPi())
-            sb.Append(b);
-        File.WriteAllText($"pi{digits}digits.txt", sb.ToString());
-        Console.WriteLine($"Done!\nDigits of pi are stored in 'pi{digits}digits.txt'");
+            Console.Write(b);
+        Console.WriteLine();
     }
 }
